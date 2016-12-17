ca609cedfa24ff889094e66e0a6a3e2101b456ea
martmists <martmists@gmail.com>
2016-12-17 09:24:17 +0100

    Fix __all__ and `shorten_urls`

--- owo.py
+++ owo.py
@@ -3,7 +3,7 @@ import json
 import base64
 import io
 
-__all__ = ["upload_file","shorten_url"]
+__all__ = ["upload_files","shorten_urls","async_upload_files","async_shorten_urls"]
 
 BASE_URL = "https://api.whats-th.is"
 IMAGE_PATH = "/upload/pomf"
@@ -20,11 +20,11 @@ def upload_files(key:str, *files: str):
     response = requests.post(BASE_URL+IMAGE_PATH, files=multipart, params={"key":key})
         
     if response.status_code != 200:
-        raise ValueError("Expected 200, got {}\n{}".format(response.status, response.text))
+        raise ValueError("Expected 200, got {}\n{}".format(response.status_code, response.text))
 
     return {item["name"]: "owo.whats-th.is/"+item["url"] for item in response.json()["files"]}
 
-def shorten_urls(key:str, urls:str):
+def shorten_urls(key:str, *urls:str):
     try:
         import requests
     except ImportError:
@@ -37,7 +37,7 @@ def shorten_urls(key:str, urls:str):
         response = requests.get(BASE_URL+URL_PATH, params={"action":"shorten","url":url, "key":key})
     
         if response.status_code != 200:
-            raise ValueError("Expected 200, got {}\n{}".format(response.status, response.text))
+            raise ValueError("Expected 200, got {}\n{}".format(response.status_code, response.text))
         
         results.append(response.text)
         
@@ -54,16 +54,16 @@ async def async_upload_files(key:str, *files: str, loop=None):
     
     with aiohttp2.MultipartWriter('form-data') as mp:
         for file in files:
-            with open(file,"rb") as f:
-                part = mp.append(io.BytesIO(f.read())) # Insert the files now so we don't get a `ValueError: read of closed file`
-                part.set_content_disposition('form-data', should_quote=False, name='files[]', filename=file)
+            part = mp.append(open(file, "rb"))
+            part.set_content_disposition('form-data', should_quote=False, name='files[]', filename=file)
         async with aiohttp.ClientSession(loop=loop) as session:
             async with session.post(BASE_URL+IMAGE_PATH, data=mp, params={"key":key}) as response:
                 if response.status != 200:
                     raise ValueError("Expected 200, got {}\n{}".format(response.status, await response.text()))
-                print(await response.text())
-                #item = (await response.json())["files"]
-                #results[item["name"]] = "owo.whats-th.is/"+item["url"]
+                for item in (await response.json())["files"]:
+                    if item.get("error") is True:
+                        raise ValueError("Expected 200, got {}\n{}".format(item["errorcode"], item["description"]))
+                    results[item["name"]] = "owo.whats-th.is/"+item["url"]
             
     return results
 
@@ -80,6 +80,6 @@ async def async_shorten_urls(key:str, *urls:str, loop=None):
             async with session.get(BASE_URL+URL_PATH, params={"action":"shorten","url":url, "key":key}) as response:
                 if response.status != 200:
                     raise ValueError("Expected 200, got {}\n{}".format(response.status, await response.text()))
-            results.append(await response.text())
+                results.append(await response.text())
         
     return results
