4687b91c125db4008d7f9bc45072de1ed63d2b66
martmists <martmists@gmail.com>
2016-12-17 15:32:08 +0100

    Updates
    - Remove a lot of whitespace
    - Create a `Client` class so you don't have to pass `api_key` and/or
    `loop` constantly
    - Made code more readable

--- README.md
+++ README.md
@@ -1,3 +1,30 @@
 # OwO.whats-th.is wrapper for python
 
-Note: `async_upload_files` does NOT work yet.
+Installation:
+
+Step 1: Get an API key
+Step 2: `pip install http://github.com/whats-this/owo.py/archive/master.zip`
+
+
+Usage:
+
+```python
+
+import owo
+
+>>> owo.shorten_urls(API_KEY, "url1","url2")
+["shortened url 1","shortened url 2"]
+
+>>> owo.upload_files(API_KEY, "file.png", "file.py")
+{"file.png": "url", "file.py": "url"}
+
+>>> import asyncio
+>>> loop = asyncio.get_event_loop()
+
+>>> loop.run_until_complete(owo.async_shorten_urls(API_KEY, "url1","url2", loop=loop))
+["shortened url 1","shortened url 2"]
+
+>>> loop.run_until_complete(owo.async_upload_files(API_KEY, "file.png", "file.py", loop=loop))
+{"file.png": "url", "file.py": "url"}
+
+```
--- owo/owo.py
+++ owo/owo.py
@@ -3,7 +3,7 @@ import json
 import base64
 import io
 
-__all__ = ["upload_files","shorten_urls","async_upload_files","async_shorten_urls"]
+__all__ = ["upload_files","shorten_urls","async_upload_files","async_shorten_urls","Client"]
 
 BASE_URL = "https://api.whats-th.is"
 IMAGE_PATH = "/upload/pomf"
@@ -13,58 +13,78 @@ def upload_files(key:str, *files: str):
     try:
         import requests
     except ImportError:
-        raise ImportError("Please install the `requests` module to use this function")
-    
-    multipart = [("files[]", (file, open(file, "rb"), mimetypes.guess_type(file)[0])) for file in files]
-    
-    response = requests.post(BASE_URL+IMAGE_PATH, files=multipart, params={"key":key})
-        
-    if response.status_code != 200:
-        raise ValueError("Expected 200, got {}\n{}".format(response.status_code, response.text))
+    raise ImportError("Please install the `requests` module to use this function")
 
-    return {item["name"]: "owo.whats-th.is/"+item["url"] for item in response.json()["files"]}
+    multipart = [(
+        "files[]",
+        (
+            file,
+            open(file, "rb"),
+            mimetypes.guess_type(file)[0]
+        ))
+        for file in files]
+
+    response = requests.post(BASE_URL+IMAGE_PATH, files=multipart, params={"key":key})
+
+    if response.status_code != 200:
+        raise ValueError("Expected 200, got {}\n{}".format(
+            response.status_code, response.text))
+
+    return {item["name"]: "https://owo.whats-th.is/"+item["url"]
+            for item in response.json()["files"]}
 
 def shorten_urls(key:str, *urls:str):
     try:
         import requests
     except ImportError:
-        raise ImportError("Please install the `requests` module to use this function")
-    
+        raise ImportError("Please install the `requests` module to use this functio*n")
+
     # Make the request
     results = []
-    
+
     for url in urls:
         response = requests.get(BASE_URL+URL_PATH, params={"action":"shorten","url":url, "key":key})
-    
-        if response.status_code != 200:
-            raise ValueError("Expected 200, got {}\n{}".format(response.status_code, response.text))
-        
-        results.append(response.text)
-        
+
+    if response.status_code != 200:
+        raise ValueError("Expected 200, got {}\n{}".format(
+            response.status_code, response.text))
+
+    results.append(response.text)
+
     return results
-    
+
 async def async_upload_files(key:str, *files: str, loop=None):
     try:
         from . import aiohttp2
         import aiohttp
     except ImportError:
         raise ImportError("Please install the `aiohttp` module to use this function")
-    
+
     results = {}
-    
+
     with aiohttp2.MultipartWriter('form-data') as mp:
         for file in files:
             part = mp.append(open(file, "rb"))
-            part.set_content_disposition('form-data', should_quote=False, name='files[]', filename=file)
+            part.set_content_disposition(
+                'form-data',
+                should_quote=False,
+                name='files[]',
+                filename=file
+            )
+
         async with aiohttp.ClientSession(loop=loop) as session:
             async with session.post(BASE_URL+IMAGE_PATH, data=mp, params={"key":key}) as response:
                 if response.status != 200:
-                    raise ValueError("Expected 200, got {}\n{}".format(response.status, await response.text()))
+                    raise ValueError("Expected 200, got {}\n{}".format(
+                        response.status, await response.text()))
+
                 for item in (await response.json())["files"]:
                     if item.get("error") is True:
-                        raise ValueError("Expected 200, got {}\n{}".format(item["errorcode"], item["description"]))
-                    results[item["name"]] = "owo.whats-th.is/"+item["url"]
-            
+                        raise ValueError("Expected 200, got {}\n{}".format(
+                            item["errorcode"], item["description"]))
+
+                    results[item["name"]] = "https://owo.whats-th.is/"+item["url"]
+
     return results
 
 async def async_shorten_urls(key:str, *urls:str, loop=None):
@@ -72,14 +92,38 @@ async def async_shorten_urls(key:str, *urls:str, loop=None):
         import aiohttp
     except ImportError:
         raise ImportError("Please install the `aiohttp` module to use this function")
-    
+
     results = []
-    
+
     async with aiohttp.ClientSession(loop=loop) as session:
         for url in urls:
             async with session.get(BASE_URL+URL_PATH, params={"action":"shorten","url":url, "key":key}) as response:
                 if response.status != 200:
-                    raise ValueError("Expected 200, got {}\n{}".format(response.status, await response.text()))
-                results.append(await response.text())
-        
+                    raise ValueError("Expected 200, got {}\n{}".format(
+                        response.status, await response.text()))
+
+            results.append(await response.text())
+
     return results
+
+class Client:
+    """
+    In case you want to make multiple requests
+    without constantly passing `api_key` and/or `loop`
+    """
+
+    def __init__(self, api_key:str, loop=None):
+        self.key = api_key
+        self.loop = loop
+
+    def upload_files(self, *files: str):
+        return upload_files(self.key, *files)
+
+    def shorten_urls(self, *urls: str):
+        return shorten_urls(self.key, *urls)
+
+    async def async_upload_files(self, *files: str):
+        return async_upload_files(self.key, *files, loop=self.loop)
+
+    async def async_shorten_urls(self, *urls: str):
+        return async_shorten_urls(self.key, *urls, loop=self.loop)
