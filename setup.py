b4c39d5f35fcbd81fd4260ed7e8a95c59886d604
martmists <martmists@gmail.com>
2017-01-02 18:57:10 +0100

    Fix scripts
    im new to this dont hate

--- setup.py
+++ setup.py
@@ -30,6 +30,6 @@ setup(
     ],
     install_requires=[],
     packages=find_packages(),
-	scripts=["owo/owo-cli", "owo/owo-bg"],
+	scripts=["lib/owo/owo-cli", "lib/owo/owo-bg"],
     keywords='api wrapper owo',
 )
