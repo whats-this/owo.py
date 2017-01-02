178ac037c85704b386efe206984bf01abb900477
martmists <martmists@gmail.com>
2017-01-02 16:47:05 +0100

    add two more domains
    https://this.is-a.dog/
    https://deanis.sexy/

--- owo/owo.py
+++ owo/owo.py
@@ -1,7 +1,10 @@
 import mimetypes
 import os.path
+import sys
 
 from functools import lru_cache
+
+PY_VERSION = sys.version_info.major
 
 __all__ = ["upload_files", "shorten_urls",
            "async_upload_files", "async_shorten_urls",
@@ -18,7 +21,8 @@ SHORTEN_STANDARD = "https://uwu.whats-th.is/"
 UPLOAD_BASES = ("https://owo.whats-th.is/", "https://i.am-a.ninja/",
                 "https://buttsare.sexy/", "https://nyanyanya.moe/",
                 "https://all.foxgirlsare.sexy/", "https://i.stole-a-me.me/",
-                "https://can-i-ask-dean-on-a.date/")
+                "https://can-i-ask-dean-on-a.date/", "https://this.is-a.dog/",
+				"https://deanis.sexy/")
 
 SHORTEN_BASES = ("https://awau.moe/", "https://uwu.whats-th.is/")
 
@@ -106,84 +110,90 @@ def shorten_urls(key: str, *urls: str, verbose=False):
     return results
 
 
-@lru_cache()
-async def async_upload_files(key: str, *files: str, loop=None, verbose=False):
-    try:
-        from . import aiohttp2
-        import aiohttp
-    except ImportError:
-        raise ImportError("Please install the `aiohttp` module "
-                          "to use this function")
+if PY_VERSION == 3:
+    import asyncio
 
-    results = {}
+    @lru_cache()
+    @asyncio.coroutine
+    def async_upload_files(key: str, *files: str, loop=None, verbose=False):
+        try:
+            from . import aiohttp2
+            import aiohttp
+        except ImportError:
+            raise ImportError("Please install the `aiohttp` module "
+                              "to use this function")
 
-    for file in files:
-        check_size(file)
+        results = {}
 
-    with aiohttp2.MultipartWriter('form-data') as mp:
         for file in files:
-            part = mp.append(open(file, "rb"))
-            part.set_content_disposition(
-                'form-data',
-                should_quote=False,
-                name='files[]',
-                filename=file
-            )
+            check_size(file)
 
-        async with aiohttp.ClientSession(loop=loop) as session:
-            async with session.post(BASE_URL+UPLOAD_PATH, data=mp,
-                                    params={"key": key}) as response:
-                if response.status != 200:
-                    raise ValueError("Expected 200, got {}\n{}".format(
-                        response.status, await response.text()))
+        with aiohttp2.MultipartWriter('form-data') as mp:
+            for file in files:
+                part = mp.append(open(file, "rb"))
+                part.set_content_disposition(
+                    'form-data',
+                    should_quote=False,
+                    name='files[]',
+                    filename=file
+                )
 
-                for item in (await response.json())["files"]:
-                    if item.get("error") is True:
+            with aiohttp.ClientSession(loop=loop) as session:
+                with (
+                    yield from session.post(BASE_URL+UPLOAD_PATH, data=mp,
+                                            params={"key": key})) as response:
+                    if response.status != 200:
                         raise ValueError("Expected 200, got {}\n{}".format(
-                            item["errorcode"], item["description"]))
+                            response.status, (yield from response.text())))
 
-                    if verbose:
-                        results[item["name"]] = {
-                            base: base+item["url"]
-                            for base in UPLOAD_BASES
-                        }
+                    for item in (yield from response.json())["files"]:
+                        if item.get("error") is True:
+                            raise ValueError("Expected 200, got {}\n{}".format(
+                                item["errorcode"], item["description"]))
 
-                    else:
-                        results[item["name"]] = UPLOAD_STANDARD+item["url"]
+                        if verbose:
+                            results[item["name"]] = {
+                                base: base+item["url"]
+                                for base in UPLOAD_BASES
+                            }
 
-    return results
+                        else:
+                            results[item["name"]] = UPLOAD_STANDARD+item["url"]
 
+        return results
 
-@lru_cache()
-async def async_shorten_urls(key: str, *urls: str, loop=None, verbose=False):
-    try:
-        import aiohttp
-    except ImportError:
-        raise ImportError("Please install the `aiohttp` module "
-                          "to use this function")
+    @lru_cache()
+    @asyncio.coroutine
+    def async_shorten_urls(key: str, *urls: str, loop=None, verbose=False):
+        try:
+            import aiohttp
+        except ImportError:
+            raise ImportError("Please install the `aiohttp` module "
+                              "to use this function")
 
-    results = []
+        results = []
 
-    async with aiohttp.ClientSession(loop=loop) as session:
-        for url in urls:
-            async with session.get(BASE_URL+SHORTEN_PATH,
-                                   params={"action": "shorten",
-                                           "url": url,
-                                           "key": key}) as response:
-                if response.status != 200:
-                    raise ValueError("Expected 200, got {}\n{}".format(
-                        response.status, await response.text()))
+        with aiohttp.ClientSession(loop=loop) as session:
+            for url in urls:
+                with (
+                    yield from session.get(BASE_URL+SHORTEN_PATH,
+                                           params={"action": "shorten",
+                                                   "url": url,
+                                                   "key": key})) as response:
+                    if response.status != 200:
+                        raise ValueError("Expected 200, got {}\n{}".format(
+                            response.status, (yield from response.text())))
 
-            path = (await response.text()).split("/")[-1]
-            if verbose:
-                results.append({
-                    base: base+path
-                    for base in SHORTEN_BASES
-                })
-            else:
-                results.append(SHORTEN_STANDARD + path)
+                path = (yield from response.text()).split("/")[-1]
+                if verbose:
+                    results.append({
+                        base: base+path
+                        for base in SHORTEN_BASES
+                    })
+                else:
+                    results.append(SHORTEN_STANDARD + path)
 
-    return results
+        return results
 
 
 class Client:
@@ -206,10 +216,13 @@ class Client:
     def shorten_urls(self, *urls: str):
         return shorten_urls(self.key, *urls, verbose=self.verbose)
 
-    async def async_upload_files(self, *files: str):
-        return async_upload_files(self.key, *files,
-                                  loop=self.loop, verbose=self.verbose)
+    if PY_VERSION == 3:
+        @asyncio.coroutine
+        def async_upload_files(self, *files: str):
+            return async_upload_files(self.key, *files,
+                                      loop=self.loop, verbose=self.verbose)
 
-    async def async_shorten_urls(self, *urls: str):
-        return async_shorten_urls(self.key, *urls,
-                                  loop=self.loop, verbose=self.verbose)
+        @asyncio.coroutine
+        def async_shorten_urls(self, *urls: str):
+            return async_shorten_urls(self.key, *urls,
+                                      loop=self.loop, verbose=self.verbose)
