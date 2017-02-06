0e2fa3b76117553d8aac11570fe43f74d6ea6cde
martmists <martmists@gmail.com>
2017-02-06 21:04:58 +0100

    Finalize bg.py
    Fixes #5

--- owo/bg.py
+++ owo/bg.py
@@ -32,6 +32,10 @@ parser.add_argument("-u", "--url", help="Base vanity url to use",
 parser.add_argument("-v", "--verbose", help="Increase output verbosity",
                     action="store_true")
 
+# Mobile-only args
+parser.add_argument("-tts", help="tts upload notification",
+                    action="store_true")
+
 args = parser.parse_args()
 
 sent_files = os.listdir(args.path)
@@ -63,8 +67,26 @@ def main():
             except ValueError as e:
                 print("Upload failed:\n{}".format(e.args[0]))
 
+                # For this we can just use try:except
+                try:
+                    if args.tts:
+                        os.system('termux-tts-speak "Upload failed"')
+                    else:
+                        os.system('termux-toast "Upload failed"')
+                except:
+                    pass
+
             except OverflowError:
                 print("File too big: {}".format(file))
+
+                # For this we can just use try:except
+                try:
+                    if args.tts:
+                        os.system('termux-tts-speak "Upload too big"')
+                    else:
+                        os.system('termux-toast "Upload too big"')
+                except:
+                    pass
                 sent_files.append(file)
 
             else:
@@ -75,28 +97,34 @@ def main():
                     # Mobile devices
 
                     try:
-						# os.system won't raise an error
+                        # os.system won't raise an error
                         subprocess.run(
-							shlex.split((
-								"termux-notification "
-								'--title "File uploaded" '
-								'--content "{0}" '
-								'--button1 "Copy link" '
-								'--button1-action "termux-clipboard-set {0}" '
-								'--button2 "Share" '
-								'--button2-action "termux-share {0}" '
-							).format(url))
-						)
-						
+                            shlex.split((
+                                "termux-notification "
+                                '--title "File uploaded" '
+                                '--content "{0}" '
+                                '--button1 "Copy link" '
+                                '--button1-action "termux-clipboard-set {0}" '
+                                '--button2 "Share" '
+                                '--button2-action "termux-share {0}" '
+                            ).format(url))
+                        )
+
+                        if args.tts:
+                            # We don't need a try:catch for this
+                            os.system('termux-tts-speak "Upload success"')
+                        else:
+                            os.system('termux-toast "Upload success"')
+
                         print_v("Sent notification")
                     except FileNotFoundError:
-						# termux-api not installed
+                        # termux-api not installed
                         print("File uploaded: {}, URL: {}".format(
-							file, url))
+                            file, url))
                 else:
-					# Non-mobile devices
+                    # Non-mobile devices
                     print("File uploaded: {}, URL: {}".format(
-						file, url))
+                        file, url))
 
 
 if __name__ == "__main__":
